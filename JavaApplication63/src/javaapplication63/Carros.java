5e181879b610f979864ac64f4845918c8605bbdb
stevenarias65 <stevenarias65@gmail.com>
2024-11-19 19:51:25 -0600

    Semana 12

---
+++ JavaApplication63/src/javaapplication63/Carros.java
@@ -0,0 +1,28 @@
+
+package javaapplication63;
+
+
+public class Carros {
+    
+    String tipoMotor;
+    String marca;
+    String placa;
+    String color;
+    int cc;
+    boolean arosLujo;
+    int tamañoAro;
+    String estilo;
+    
+    public void ActualizarAuto(){
+        
+    }
+    
+    public void eliminarAuto(){
+        
+    }
+    
+    
+    
+    
+    
+}
---
+++ JavaApplication63/src/javaapplication63/JavaApplication63.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication63;
+
+/**
+ *
+ * @author steven
+ */
+public class JavaApplication63 {
+
+    public static void main(String[] args) {
+        Carros auto1 = new Carros();
+        Carros auto2 = new Carros();
+        Carros auto3 = new Carros();
+        Carros auto4 = new Carros();
+        auto1.color = "Rojos";
+        auto2.color = "azul";
+        System.out.println("hola mundo");
+    }
+}
